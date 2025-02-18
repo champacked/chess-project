118d94745d763544d5c0c0101d377cc1d13ca9d9
Aman Verma <amanvijayverma07@gmail.com>
2025-02-18 12:02:48 +0000

    created the dashboard

---
+++ frontend/script.js
@@ -0,0 +1,23 @@
+    
+var config = {
+    draggable: true,
+    position: 'start',
+    dropOffBoard: 'snapback',
+    sparePieces: true,
+    pieceTheme: 'chessboard/img/chesspieces/wikipedia/{piece}.png',
+    snapSpeed: 100,
+  }  
+
+  var board = Chessboard('chessboard', config);
+
+    document.getElementById('flipBoard').addEventListener('click', function () {
+        board.flip();
+    });
+
+    document.getElementById('resetBoard').addEventListener('click', function () {
+        board.start();
+    });
+
+    document.getElementById('clearBoard').addEventListener('click', function () {
+        board.clear();
+    });
