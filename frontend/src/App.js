98b950bf41089ee41ffce2f8ee3e6e1643c9933d
default <tarunmalviya8827@gmail.com>
2025-02-25 07:30:46 +0000

    resolve draw button conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,12 @@ const API_URL = 'http://localhost:9000';
 console.log(API_URL);
 
 function App() {
+
   const [fen, setFen] = useState('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
   const [gameStatus, setGameStatus] = useState('');
   const [isGameOver, setIsGameOver] = useState(false);
   const [winner, setWinner] = useState('');
-  const [currentTurn, setCurrentTurn] = useState('w');
+  const [PlayerTurn, setPlayerTurn] = useState('');
 
   const startNewGame = useCallback(async () => {
     try {
@@ -20,10 +21,12 @@ function App() {
       setIsGameOver(false);
       setWinner('');
       setGameStatus('Game Status: Playing');
-      setCurrentTurn('w');
+      setPlayerTurn(response?.data?.turn);
+
     } catch (error) {
       console.error('Error starting new game:', error);
       setGameStatus(`Error starting new game: ${error.response?.data?.error || error.message}`);
+
     }
   }, []);
 
@@ -46,7 +49,8 @@ function App() {
     try {
       const response = await axios.post(`${API_URL}/move`, { move });
       setFen(response.data.fen);
-      
+      console.log(response.data.turn);
+      setPlayerTurn(response?.data?.turn);
       if (response.data.gameOver) {
         await checkGameResult();
       } else {
@@ -69,7 +73,8 @@ function App() {
       setGameStatus('AI is thinking...');
       const response = await axios.get(`${API_URL}/ai-move`);
       setFen(response.data.fen);
-      
+      setPlayerTurn(response?.data?.turn)
+
       if (response.data.gameOver) {
         await checkGameResult();
       } else {
@@ -101,26 +106,61 @@ function App() {
     }
   };
 
+  const handleUndoMove = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/undo-move`);
+      console.log("undo move :", response.data);
+      setIsGameOver(response.data?.gameOver);
+      setWinner(response.data.winner);
+      setGameStatus('undo move done!!!');
+      setFen(response?.data?.fen)
+    } catch (error) {
+      console.error('Error checking game result:', error);
+      if (error.code === 'ECONNREFUSED' || error.code === 'ERR_NETWORK') {
+        setGameStatus('Lost connection to the game server. Please check if the backend is running.');
+      } else {
+        setGameStatus(`Error checking game result: ${error.response?.data?.error || 'Please start a new game'}`);
+      }
+    }
+  };
+
+  const handleDrawGame = async () => {
+    try {
+      const response = await axios.post(`${API_URL}/draw-game`);
+      console.log( response.data.message);
+      setIsGameOver(response.data?.isGameDraw);
+      setGameStatus('Game is Drawn');
+     
+    } catch (error) {
+      console.error('Error checking game result:', error);
+      if (error.code === 'ECONNREFUSED' || error.code === 'ERR_NETWORK') {
+        setGameStatus('Lost connection to the game server. Please check if the backend is running.');
+      } else {
+        setGameStatus(`Error checking game result: ${error.response?.data?.error || 'Please start a new game'}`);
+      }
+    }
+  };
+
+
+
   const onDrop = (sourceSquare, targetSquare) => {
     const move = {
       from: sourceSquare,
       to: targetSquare,
       promotion: 'q' // always promote to queen for simplicity
     };
-    
+
     return makeMove(move);
   };
 
   return (
     <div className="app">
       <h1>Chess Game</h1>
-      <div className="turn-indicator">
-        <span className="turn-indicator-dot"></span>
-        Current Turn: {currentTurn === 'w' ? 'White' : 'Black'}
-      </div>
+      <button>{(PlayerTurn === 'w' && "Your's Turn" )||(PlayerTurn === 'b' && "Computer's Turn")}</button>
+
       <div className="board-container">
-        <Chessboard 
-          position={fen} 
+        <Chessboard
+          position={fen}
           onPieceDrop={onDrop}
           boardWidth={400}
           areArrowsAllowed={true}
@@ -131,13 +171,23 @@ function App() {
           animationDuration={200}
           customDropSquareStyle={{ boxShadow: 'inset 0 0 1px 4px yellow' }}
         />
+       
       </div>
-      <button onClick={startNewGame} className="new-game-btn">
-        Reset Game
-      </button>
+      <div className='game-Btn'>
+        <button onClick={startNewGame} className="new-game-btn">
+          Reset Game
+        </button>
+        <button onClick={handleUndoMove} className="new-game-btn">
+          Undo Move
+        </button>
+        <button onClick={handleDrawGame} className="new-game-btn">
+          Draw
+        </button>
+      </div>
       <div className="game-status">
         <p>{gameStatus}</p>
       </div>
+
     </div>
   );
 }
